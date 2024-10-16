93857bdd0ed01de5033d794700fee0fe0bd470d5
Dmitrity <web11rider@gmail.com>
2024-10-16 21:16:30 +0300

    Fixed some things, added 404 handling

--- docs/lab1/lab1.js
+++ docs/lab1/lab1.js
@@ -1,15 +1,24 @@
 const d = document.getElementById("display");
 var doc = "";
 
-function loadDoc(selectedDoc){
+function loadDoc(selectedDoc) {
     doc = selectedDoc;
 }
 
-function loadContent(option){
-    if (option == "result"){
-        d.innerHTML = `<object type="text/html" data="docs/${doc}/${doc}.html"></object>`;
-    }
-    else{
-        fetch(`docs/${doc}/${doc}.${option}`).then(response => response.text()).then(html => d.innerHTML = html);
-    }
+function loadContent(type, option) {
+    fetch(`docs/${doc}/${doc}.${type}`).then(response => {
+        if(!response.ok) {
+            d.textContent = "404, not found";
+        }
+        else {
+            if(option == "result"){
+                d.style.whiteSpace = "initial";
+                response.text().then(html => d.innerHTML = html)
+            }
+            else{
+                d.style.whiteSpace = "pre";
+                response.text().then(html => d.textContent = html);
+            }
+        }
+    });
 }
--- index-script.js
+++ index-script.js
@@ -1,27 +1,24 @@
 const d = document.getElementById("display");
 var doc = "";
 
-function loadDoc(selectedDoc){
+function loadDoc(selectedDoc) {
     doc = selectedDoc;
 }
 
-function loadContent(option){
-    d.style.whiteSpace = "pre";
-    switch (option) {
-        case "result":
-            d.style.whiteSpace = "initial";
-            fetch(`docs/${doc}/${doc}.html`).then(response => response.text()).then(html => d.innerHTML = html);
-            break;
-        case "html":
-            fetch(`docs/${doc}/${doc}.html`).then(response => response.text()).then(html => d.textContent= html);
-            break;
-        case "css":
-            fetch(`docs/${doc}/${doc}.css`).then(response => response.text()).then(html => d.innerHTML = html);
-            break;
-        case "js":
-            fetch(`docs/${doc}/${doc}.js`).then(response => response.text()).then(html => d.innerHTML = html);
-            break;
-        default:
-            break;
-    }
+function loadContent(type, option) {
+    fetch(`docs/${doc}/${doc}.${type}`).then(response => {
+        if(!response.ok) {
+            d.textContent = "404, not found";
+        }
+        else {
+            if(option == "result"){
+                d.style.whiteSpace = "initial";
+                response.text().then(html => d.innerHTML = html)
+            }
+            else{
+                d.style.whiteSpace = "pre";
+                response.text().then(html => d.textContent = html);
+            }
+        }
+    });
 }
