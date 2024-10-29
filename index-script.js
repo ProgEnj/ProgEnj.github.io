e0809ba212fcc7904a2301c712c739b11f53dad3
Dmitrity <web11rider@gmail.com>
2024-10-29 20:11:26 +0200

    Added query entries for user's choices and proper load with query

--- index-script.js
+++ index-script.js
@@ -1,3 +1,4 @@
+const url = new URL(window.location);
 const d = document.getElementById("display");
 const text = document.getElementById("display-text");
 const goto = document.getElementById("goto");
@@ -5,13 +6,35 @@ const nowdoc = document.getElementById("nowdoc");
 const nowopt = document.getElementById("nowopt");
 var doc = "";
 
+addEventListener("load", onLoad);
+
+function onLoad() {
+    doc = url.searchParams.get("doc");
+    type = url.searchParams.get("type");
+    if(doc != null){
+        loadDoc(doc);
+    }
+
+    if(type != null){
+        loadContent(type, "result"); // to always go to result
+    }
+}
+
 function loadDoc(selectedDoc) {
     doc = selectedDoc;
     goto.href = `docs/${doc}/${doc}.html`;
     nowdoc.innerText = doc;
+    if ('URLSearchParams' in window) {
+        url.searchParams.set("doc", doc);
+        history.pushState(null, '', url);
+    }
 }
 
 function loadContent(type, option) {
+    if ('URLSearchParams' in window) {
+        url.searchParams.set("type", type);
+        history.pushState(null, '', url);
+    }
     let path = `docs/${doc}/${doc}.${type}`;
     nowopt.innerText = type;
     fetch(path).then(response => {
@@ -20,13 +43,11 @@ function loadContent(type, option) {
         }
         else {
             if (option == "result"){
-                console.log("d")
                 text.style.display = "none";
                 d.style.display = "block";
                 d.src = path;
             }
             else {
-                console.log("text");
                 d.style.display = "none";
                 text.style.display = "block";
                 response.text().then(html => text.textContent = html);
